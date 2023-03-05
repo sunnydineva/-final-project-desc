78c217214f6903167791a53908eef3020c40b94a
sunnydineva <114307463+sunnydineva@users.noreply.github.com>
2023-03-05 23:25:37 +0200

    first commit

---
+++ src/main/java/com/example/finalprojectdesc/FinalProjectDescApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectdesc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectDescApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectDescApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/finalprojectdesc/FinalProjectDescApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectdesc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectDescApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
